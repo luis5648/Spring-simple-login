2a5fb6f9f95679bf8dbb35c1f6ace319ad0679e4
Luis Román <porta5648@gmail.com>
2019-12-09 17:02:46 -0600

    post mapping vacío

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -9,9 +11,16 @@ public class UserController {
 	
 	@GetMapping("/")
 	public String inicio() {
-	
+		System.out.println("ruta de inicio\n");
 	
 		return "index.html";
+	}
+	
+	@PostMapping("/login")
+	public String login(){
+		
+		
+		return "nothing";
 	}
 	
 	
