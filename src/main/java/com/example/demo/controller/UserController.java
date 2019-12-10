4f8d9b6e45bfee746441c779b5a593cd7463e959
Luis Román <porta5648@gmail.com>
2019-12-10 12:12:43 -0600

    Subida del formulario completa

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,29 +1,37 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.UserModel;
 
 @Controller
 public class UserController {
 	
 	
-	@GetMapping("/")
-	public String inicio() {
+	@GetMapping("/login")
+	public String inicio(Model model) {
 		System.out.println("ruta de inicio\n");
-	
+		
+		model.addAttribute("usuario", new UserModel());
+		
 		return "index.html";
 	}
 	
 	@PostMapping("/login")
-	public String login(){
-		//pendiente codigo para manejar la lógica del usuario
+	public String login(@ModelAttribute UserModel usuario ){
+		//pendiente codigo para manejar la lógica del usuario.- listo xd
 		//(usuario y contraseña) y para manejar sus respectivos valores como un objeto
-		//y así poder capturarlos mediante un formulario
+		//y así poder capturarlos mediante un formulario.- listo
 		// info: https://spring.io/guides/gs/handling-form-submission/
 		
-		return "nothing";
+		System.out.println(usuario.getPass());
+		System.out.println(usuario.getNombre());
+		
+		return "main.html";
 	}
 	
 	
---
+++ src/main/java/com/example/demo/model/UserModel.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+public class UserModel {
+	
+	
+	private String nombre;
+	private String pass;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+	
+	
+}
